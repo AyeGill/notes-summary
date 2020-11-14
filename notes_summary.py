ee1e1cfb4147f94a0a52e150fc4861c4aaf3403f
Eigil Rischel <ayegill@gmail.com>
2020-11-14 15:46:53 +0000

    Implemented "new note links" feature

--- notes_summary.py
+++ notes_summary.py
@@ -34,6 +34,7 @@ try:
     TEST = config.getboolean("settings","Test",fallback=False)
     SHOW_EXTLINKS = config.getboolean("settings","ExternalLinks",fallback=True)
     SHOW_NEWNOTES = config.getboolean("settings","NewNotes",fallback=True)
+    SHOW_NOTELINKS = config.getboolean("settings","NoteLinks",fallback=True)
     MAILGUN_APIKEY = config.get("settings","MailgunApiKey")
     MAILGUN_DOMAIN = config.get("settings","MailgunDomain")
     TARGET_MAIL = config.get("settings","TargetMail")
@@ -179,13 +180,48 @@ def is_note(filename):
 
 def get_title(filename):
     org = orgparse.load(filename)
-    return org.get_property("title")
+    if org.get_property("title"):
+        return org.get_property("title")
+    return filename #fallback
 
 def get_newnotes(lines):
     newfiles = find_new_files(lines)
     newnotenames = [get_title(f) for f in newfiles if is_note(f)]
     return display_list("NEW NOTES",newnotenames)
 
+###################
+#New internal links
+###################
+
+# this is a bit tricky because we need to
+# 1) look for new internal links, like [[file:...]], and also:
+# 2) For each such link, find out which file it was added to.
+# We do this by going through lines and maintaining the file we're in as state.
+
+diff_start_regex = re.compile("diff --git [^\n]* b\/([^\n]*)")
+file_link_regex = re.compile("\[\[file:(.*)\]\[(.*)\]\]")
+
+def get_new_int_links(lines):
+    curr_file = None
+    intlinks = []
+    for line in lines:
+        f = diff_start.regex.match(line)
+        if f:
+            curr_file = r.group(1)
+
+        m = file_link_regex.match(line)
+        if m:
+            linked_file = r.group(1)
+            alias = r.group(2)
+            intlinks.append((curr_file,linked_file,alias))
+    return intlinks
+
+def get_notelinks(lines):
+    intlinks = get_new_int_links(lines)
+    notelinks = [get_title(domain) + " -> " + get_title(target)
+                 for (domain,target,_) in intlinks
+                 if is_note(domain) & is_note(target)]
+    return display_list("NEW NOTE LINKS",notelinks)
 
 ######################
 #TYING THINGS TOGETHER
@@ -198,4 +234,6 @@ if SHOW_EXTLINKS:
 if SHOW_NEWNOTES:
     msg += get_newnotes(diff_lines)
 
+if SHOW_NOTELINKS:
+    msg += get_notelinks(diff_lines)
 sendmail(msg)
